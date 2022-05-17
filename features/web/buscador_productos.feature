54ae287a45c7df448ef34b0f149b55ef4d323417
lorblak <kvasquez@fi.uba.ar>
2022-05-16 22:19:55 -0300

    feat: new steps were added

--- features/web/buscador_productos.feature
+++ features/web/buscador_productos.feature
@@ -1,5 +1,10 @@
 Feature: Buscador de productos
     Como usuario quiero cargar productos desde el buscador
+    
+    Scenario: agregar producto a carrito de compras y continuar en pantalla de productos
+        Given carga de producto 
+        When carga de producto al carrito y continuar
+        Then se tienen resultados para el producto Shirt
 
     Scenario: carga de producto sin haber ingresado valor en buscador
         Given pagina principal de automation practice
@@ -9,9 +14,4 @@ Feature: Buscador de productos
     Scenario: carga de producto Shirt
         Given pagina principal de automation practice
         When cargo producto Shirt en el buscador
-        Then tengo resultados para el producto Shirt
-
-    Scenario: agregar producto a carrito de compras y continuar en pantalla de productos
-        Given carga de producto 
-        When carga de producto al carrito y continuar
         Then se tienen resultados para el producto Shirt
--- stepDefinitions/web/automation_practice.steps.ts
+++ stepDefinitions/web/automation_practice.steps.ts
@@ -1,1 +1,46 @@
-import { Given, Then, When } from '@cucumber/cucumber';
+import { Given, Then, When } from "@cucumber/cucumber";
+
+Given(/^pagina principal de automation practice$/, async () => {
+  await browser.url("http://automationpractice.com/index.php");
+  await browser.maximizeWindow();
+});
+
+Given(/^carga de producto$/, async () => {
+  await browser.url("http://automationpractice.com/index.php");
+  await browser.maximizeWindow();
+
+  const searchInput = await $(" #search_query_top");
+  const searchBtn = await $("//button[@name='submit_search']");
+
+  searchInput.setValue("Shirt");
+  searchBtn.click();
+
+  const listBtn = await $("#list");
+  listBtn.click();
+});
+
+When(/^se hace busqueda con campo search vacio$/, async () => {
+  const searchBtn = await $("//button[@name='submit_search']");
+  searchBtn.click();
+});
+
+When(/^cargo producto Shirt en el buscador$/, async () => {
+  const searchInput = await $(" #search_query_top");
+  const searchBtn = await $("//button[@name='submit_search']");
+  searchInput.setValue("Shirt");
+  searchBtn.click();
+});
+
+When(/^carga de producto al carrito y continuar$/, async () => {});
+
+Then(/^el sistema pide que se ingrese un valor$/, async () => {
+  const alertWarning = await $(".alert.alert-warning");
+  expect(alertWarning.getText()).toHaveTextContaining(
+    "Please enter a search keyword"
+  );
+});
+
+Then(/^se tienen resultados para el producto Shirt$/, async () => {
+  const resultHeader = await $(".lighter");
+  expect(resultHeader.getText()).toHaveTextContaining("Shirt");
+});
