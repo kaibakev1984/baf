7dd7c51cf9ca1dbf64ce5094ae221223d88109c6
lorblak <kvasquez@fi.uba.ar>
2022-05-17 20:30:58 -0300

    feat(features/web/contacto.feature): se agregaron steps para contacto

--- stepDefinitions/web/automation_practice.steps.ts
+++ stepDefinitions/web/automation_practice.steps.ts
@@ -22,3 +22,42 @@ Then(/^tengo resultados para el producto (.+)$/, async (producto: string) => {
     const headerResult = await AutomationPractice.headerResult;
     expect(headerResult.getText()).toHaveTextContaining(producto);
   });
+
+Given(/^acceso a pagina de datos de contacto$/, async () => {
+    const contactUsBtn = await $('#contact-link');
+
+    contactUsBtn.click();
+  });
+
+When(/^envio de mensaje sin completar datos$/, async () => {
+    const sendBtn = await $("//span[normalize-space()='Send']");
+    sendBtn.click();
+  });
+
+When(/^envio de mensaje con datos$/, async (dataTable: any) => {
+    const datos: any = dataTable.hashes()[0];
+
+    const subjectHeading = await $('#id_contact');
+    const emailInput = await $('#email');
+    const orderReferenceInput = await $('#id_order');
+    const messageInput = await $('#message');
+    const sendBtn = await $("//span[normalize-space()='Send']");
+
+    emailInput.setValue(datos['email']);
+    orderReferenceInput.setValue(datos['pedidoReferencia']);
+    messageInput.setValue(datos['mensaje']);
+
+    await subjectHeading.selectByAttribute("value", "1");
+    sendBtn.click();
+
+  });
+
+Then(/^se alerta que la operacion es invalida$/, async () => {
+    const alertWarning = await $("//p[normalize-space()='There is 1 error']");
+    expect(alertWarning.getText()).toHaveTextContaining('There is 1 error');
+  });
+
+Then(/^el envio de mensaje fue exitoso$/, async () => {
+    const successfulAlert = await $('.alert.alert-success');
+    expect(successfulAlert.getText()).toHaveTextContaining('Your message has been successfully sent to our team.');
+  });
